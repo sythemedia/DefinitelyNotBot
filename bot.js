79081b70a423d749e69b0fe3b7fb2d16747376f5
sythemedia <36555749+sythemedia@users.noreply.github.com>
2019-01-02 17:06:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ bot.login(botToken);
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.username}`);
-    client.user.setActivity("Watching Servers", {type: "WATCHING"});
+    client.user.setActivity("TESTING", {type: "WATCHING"});
 })
 
 //If there isn't a reaction for every role, scold the user!
