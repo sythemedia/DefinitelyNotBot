5a4bcf05647f92669664b7b9a68d204eea7ca4cc
sythemedia <36555749+sythemedia@users.noreply.github.com>
2019-01-01 22:16:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
+ 
+
 client.on('ready', () => {
+
     console.log('I am ready!');
+
 });
+
+ 
 
 client.on('message', message => {
-    if (message.content === '!eval') {
-    	message.reply('TypeError: Cannot read property '' of null');
-  	}
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
 
+ 
+
 // THIS  MUST  BE  THIS  WAY
-client.login(NTI5ODQ5NDk5NjQwNzkxMDQw.Dw28PQ.2XfZq7LnTKnj4kkCt31vYOL9Oi0);
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
